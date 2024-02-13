9078763d906d2ef6bf198b67e055c67671b26bcd
prince <sinothileprinceton@gmail.com>
2024-02-13 20:55:26 +0200

    todo list app project

---
+++ app/src/main/java/com/example/a2doapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.a2doapp;
+
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.a2doapp.Adapter.ToDoAdapter;
+import com.example.a2doapp.Model.ToDoModel;
+import com.example.a2doapp.Utils.DatabaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnDialogCloseListener{
+
+    private RecyclerView mRecyclerview;
+    private DatabaseHelper myDB;
+    private List<ToDoModel> mList;
+    private ToDoAdapter adapter;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerview = findViewById(R.id.recyclerView);
+        fab = findViewById(R.id.fab);
+        myDB = new DatabaseHelper(MainActivity.this);
+        mList = new ArrayList<>();
+        adapter = new ToDoAdapter(myDB, MainActivity.this);
+
+        mList = myDB.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager() , AddNewTask.TAG);
+            }
+        });
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerViewTouchHelper(adapter));
+        itemTouchHelper.attachToRecyclerView(mRecyclerview);
+    }
+
+
+    @Override
+    public void onDialogClose(DialogInterface dialogInterface) {
+        mList = myDB.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+        adapter.notifyDataSetChanged();
+
+        mRecyclerview.setHasFixedSize(true);
+        mRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerview.setAdapter(adapter);
+
+    }
+}
---
+++ app/src/main/java/com/example/a2doapp/OnDialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.a2doapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+    void onDialogClose(DialogInterface dialogInterface);
+}
